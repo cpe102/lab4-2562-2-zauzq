5fdc7dd773a92d5487b4e024b7c9cb49101c686d
lab302-24 <lab302-24@labeng.local>
2020-01-03 15:38:44 +0700

    lad4_1.cpp

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+double funtion(double x){
+    double y=(3*x*x*x)+(2*2.71828)+pow(2,((2*x)+1))+pow(((x*x)+1),0.5);
+    return y;
+}
+int main()
+{
+    double a;
+    cout<<"Enter x: ";
+    cin>>a;
+    cout<<"\nResult y: "<<funtion(a);
+
+    return 0;
+}
